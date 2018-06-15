49086f7d3f1182ce20123462c70eac80035c9765
hietnv <hiet@tel.red>
2018-06-15 16:17:49 +0700

    Add install requirements script

---
+++ install_dep.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+apt-get update  # To get the latest package lists
+
+sudo apt-get install -y python-pip chromium-browser
+sudo pip install --upgrade pip
+
+sudo pip install selenium bs4 requests
+
+#etc.ooooffffjfkfkđfg
